0cb0d84f4c4ba50fed0cf377d40a7a3cd48ffa03
gsawers <gregory.sawers@mindbodyonline.com>
2014-07-18 11:12:28 -0700

    Added Dynamic List Adapter constructors
    Added ability to build a Dynamic List Adapter with the parent Id or the
    parent class.

--- lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/MindbodyViews/DynamicListAdapter.java
+++ lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/MindbodyViews/DynamicListAdapter.java
@@ -1,9 +1,10 @@
 package com.mindbodyonline.ironhide.Infrastructure.MindbodyViews;
 
 import android.view.View;
-import com.google.android.apps.common.testing.ui.espresso.DataInteraction;
+
 import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
 import com.mindbodyonline.ironhide.Infrastructure.Extensions.MindbodyViewMatchers;
+
 import org.hamcrest.Matcher;
 
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
@@ -22,12 +23,26 @@ import static org.hamcrest.Matchers.is;
 public class DynamicListAdapter<T> extends MindbodyView<T> {
 
     private int parentId;
+    private Class parentClass;
     private Class itemType;
 
     public DynamicListAdapter(Class<T> type, Class itemType) {
         this.type = type;
         this.parentId = parentId;
         this.itemType = itemType;
+    }
+
+    public DynamicListAdapter(Class<T> type, Class itemType, int parentId){
+        this.type = type;
+        this.parentId = parentId;
+        this.itemType = itemType;
+    }
+
+    public DynamicListAdapter(Class<T> type, Class itemType, int parentId, Class parentClass){
+        this.type = type;
+        this.parentId = parentId;
+        this.itemType = itemType;
+        this.parentClass = parentClass;
     }
 
     public DynamicListAdapter(Class<T> type, Class itemType, Matcher<View> selector) {
@@ -40,5 +55,24 @@ public class DynamicListAdapter<T> extends MindbodyView<T> {
                 allOf(ViewMatchers.isDisplayed(),
                         MindbodyViewMatchers.instanceOf(itemType),
                         MindbodyViewMatchers.hasIndex(index)));
+    }
+
+    public Clickable<T> getItemFromParentWithId(int index){
+
+        return new Clickable<T>(type,
+                allOf(ViewMatchers.isDisplayed(),
+                        MindbodyViewMatchers.instanceOf(itemType),
+                        MindbodyViewMatchers.hasIndex(index)
+                        ,ViewMatchers.isDescendantOfA(ViewMatchers.withId(parentId))));
+    }
+
+
+    public Clickable<T> getItemFromParentWithClass(int index){
+
+        return new Clickable<T>(type,
+            allOf(ViewMatchers.isDisplayed(),
+                MindbodyViewMatchers.instanceOf(type),
+                MindbodyViewMatchers.hasIndex(1)
+                , ViewMatchers.isDescendantOfA( MindbodyViewMatchers.instanceOf(parentClass))));
     }
 }
