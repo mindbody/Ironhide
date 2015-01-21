0cc8fca3bd8b5f30e9e02a339c4f4e96967a6ac7
Kyle Lozier <kyle.lozier@mindbodyonline.com>
2015-01-21 15:08:24 -0800

    Removed redundancy in constructors and methods

--- lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/DynamicListAdapter.java
+++ lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/DynamicListAdapter.java
@@ -1,7 +1,10 @@
 package com.mindbodyonline.ironhide.Infrastructure.IronhideViews;
 
+import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.Root;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.view.View;
 
 import com.mindbodyonline.ironhide.Infrastructure.Extensions.BaseViewMatchers;
 import com.mindbodyonline.ironhide.PageObjects.PageObject;
@@ -9,6 +12,13 @@ import com.mindbodyonline.ironhide.PageObjects.PageObject;
 import org.hamcrest.Matcher;
 
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static com.mindbodyonline.ironhide.Infrastructure.Extensions.BaseViewMatchers.hasIndex;
+import static com.mindbodyonline.ironhide.Infrastructure.Extensions.BaseViewMatchers.instanceOf;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Extends BaseView
@@ -21,74 +31,43 @@ import static org.hamcrest.Matchers.allOf;
 public class DynamicListAdapter<T extends PageObject> {
 
     private Class<T> type;
-    private int parentId;
-    private Class parentClass;
-    private Class itemType;
+    private Matcher<View> childMatcher;
 
     public DynamicListAdapter(Class<T> type, Class itemType) {
-        this.type = type;
-        this.itemType = itemType;
+        this(type, instanceOf(itemType));
     }
 
-    public DynamicListAdapter(Class<T> type, Class itemType, int parentId){
-        this.type = type;
-        this.parentId = parentId;
-        this.itemType = itemType;
+    public DynamicListAdapter(Class<T> type, Class itemType, int parentId) {
+        this(type, allOf(instanceOf(itemType),
+                         isDescendantOfA( withId(parentId) )));
     }
 
-    public DynamicListAdapter(Class<T> type, Class itemType, Class parentClass){
-        this.type = type;
-        this.itemType = itemType;
-        this.parentClass = parentClass;
+    public DynamicListAdapter(Class<T> type, Class itemType, Class parentClass) {
+        this(type, allOf(instanceOf(itemType),
+                         isDescendantOfA( instanceOf(parentClass) )));
     }
 
-    public DynamicListAdapter(Class<T> type, Class itemType, int parentId, Class parentClass){
-        this.type = type;
-        this.parentId = parentId;
-        this.itemType = itemType;
-        this.parentClass = parentClass;
+    public DynamicListAdapter(Class<T> type, Class itemType, int parentId, Class parentClass) {
+        this(type, allOf(instanceOf(itemType),
+                         isDescendantOfA(allOf(
+                                withId(parentId),
+                                instanceOf(parentClass) ))));
     }
 
+    private DynamicListAdapter(Class<T> type, Matcher<View> childMatcher) {
+        this.type = type;
+        this.childMatcher = allOf(isDisplayed(), childMatcher);
+    }
 
     public Clickable<T> getItemAt(int index) {
-        return new Clickable<T>(type,
-                allOf(ViewMatchers.isDisplayed(),
-                        BaseViewMatchers.instanceOf(itemType),
-                        BaseViewMatchers.hasIndex(index)));
-    }
-
-    public Clickable<T> getItemFromParentWithId(int index) {
-        return new Clickable<T>(type,
-                allOf(ViewMatchers.isDisplayed(),
-                        BaseViewMatchers.instanceOf(itemType),
-                        BaseViewMatchers.hasIndex(index),
-                        ViewMatchers.isDescendantOfA(ViewMatchers.withId(parentId))));
-    }
-
-    public Clickable<T> getItemFromParentWithClass(int index) {
-        return new Clickable<T>(type,
-            allOf(ViewMatchers.isDisplayed(),
-                    BaseViewMatchers.instanceOf(itemType),
-                    BaseViewMatchers.hasIndex(index),
-                    ViewMatchers.isDescendantOfA( BaseViewMatchers.instanceOf(parentClass))));
+        return getItemMatching(hasIndex(index));
     }
 
     public Clickable<T> getItemFromText(String text) {
-        return new Clickable<T>(type,
-                allOf(ViewMatchers.isDisplayed(),
-                        BaseViewMatchers.instanceOf(itemType),
-                        ViewMatchers.withText(text)));
+        return getItemMatching(withText(text));
     }
 
-    @Override
-    public DynamicListAdapter<T> changeRoot() {
-        super.changeRoot();
-        return this;
-    }
-
-    @Override
-    public DynamicListAdapter<T> inRoot(Matcher<Root> rootMatcher) {
-        super.inRoot(rootMatcher);
-        return this;
+    public Clickable<T> getItemMatching(Matcher<View> itemMatcher) {
+        return new Clickable<T>(type, allOf(childMatcher, itemMatcher));
     }
 }
