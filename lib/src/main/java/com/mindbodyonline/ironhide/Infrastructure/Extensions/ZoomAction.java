87323755a21beec26e671f8c8dbafd60d5690776
Kyle Lozier <kyle.lozier@mindbodyonline.com>
2015-01-30 15:53:01 -0800

    Refactored zoom to its own ViewAction class

---
+++ lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/Extensions/ZoomAction.java
@@ -0,0 +1,132 @@
+package com.mindbodyonline.ironhide.Infrastructure.Extensions;
+
+import android.annotation.TargetApi;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.support.test.espresso.InjectEventSecurityException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.v4.util.Pair;
+import android.view.MotionEvent;
+import android.view.MotionEvent.PointerCoords;
+import android.view.MotionEvent.PointerProperties;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
+import static android.view.MotionEvent.*;
+import static android.os.SystemClock.uptimeMillis;
+
+/**
+ * Created by Kyle.Lozier on 1/30/2015.
+ */
+@TargetApi(14)
+public class ZoomAction implements ViewAction {
+    
+    private static final float DEFAULT_PRESSURE = 1, DEFAULT_SIZE = 1;
+
+    // not sure why
+    private static final int EVENT_MIN_INTERVAL = 1000;
+    
+    private PointerCoords[] coordinates;
+    private PointerProperties[] properties;
+    private PointF[] steps;
+
+    private Pair<Point, Point> endPoints;
+    private long duration;
+    
+    public ZoomAction(Pair<Point, Point> startPoints, Pair<Point, Point> endPoints, long durationMillis) {
+        this.coordinates = new PointerCoords[2];
+        this.properties = new PointerProperties[2];
+        this.steps = new PointF[2];
+        this.endPoints = endPoints;
+        this.duration = durationMillis / EVENT_MIN_INTERVAL;
+        
+        coordinates[0] = getPointerCoordsFrom(startPoints.first);
+        coordinates[1] = getPointerCoordsFrom(startPoints.second);
+
+        properties[0] = generatePointerProperties(0);
+        properties[1] = generatePointerProperties(1);
+
+        steps[0] = new PointF(endPoints.first);
+        steps[0].offset(-coordinates[0].x, -coordinates[0].y);
+        steps[0].x /= duration;
+        steps[0].y /= duration;
+
+        steps[1] = new PointF(endPoints.second);
+        steps[1].offset(-coordinates[1].x, -coordinates[1].y);
+        steps[1].x /= duration;
+        steps[1].y /= duration;
+    }
+    
+    @Override
+    public Matcher<View> getConstraints() {
+        return isDisplayingAtLeast(90);
+    }
+
+    // TODO: be a little more descriptive
+    @Override
+    public String getDescription() {
+        return "zoom";
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        long startTime = uptimeMillis();
+        int action2Mod = properties[1].id << ACTION_POINTER_INDEX_SHIFT;
+
+        try {
+            uiController.injectMotionEvent(obtainWrapper(startTime, ACTION_DOWN, 1));
+            uiController.injectMotionEvent(obtainWrapper(startTime, ACTION_POINTER_DOWN + action2Mod, 2));
+
+            for (long i = 0; i < duration; i++) {
+                coordinates[0].x += steps[0].x;
+                coordinates[0].y += steps[0].y;
+                coordinates[1].x += steps[1].x;
+                coordinates[1].y += steps[1].y;
+
+                uiController.injectMotionEvent(obtainWrapper(startTime, startTime + i * EVENT_MIN_INTERVAL, ACTION_MOVE, 2));
+            }
+
+            coordinates[0].x = endPoints.first.x;
+            coordinates[0].y = endPoints.first.y;
+            coordinates[1].x = endPoints.second.x;
+            coordinates[1].y = endPoints.second.y;
+
+            uiController.injectMotionEvent(obtainWrapper(startTime, startTime + duration, ACTION_POINTER_UP + action2Mod, 2));
+            uiController.injectMotionEvent(obtainWrapper(startTime, ACTION_UP, 1));
+        } catch (InjectEventSecurityException e) {
+            // something something shut up espresso
+        }
+    }
+
+    // TODO: I like magic numbers and I cannot lie; magic numbers and I can't deny
+    private MotionEvent obtainWrapper(long startTime, int action, int num) {
+        return obtainWrapper(startTime, uptimeMillis(), action, num);
+    }
+
+    private MotionEvent obtainWrapper(long startTime, long eventTime, int action, int num) {
+        return obtain(startTime, eventTime, action, num, properties, coordinates, 0, 0, 1, 1, 0, 0, 0, 0);
+    }
+    
+    private static PointerCoords getPointerCoordsFrom(Point source) {
+        PointerCoords coords = new PointerCoords();
+        
+        coords.x = source.x;
+        coords.y = source.y;
+        coords.pressure = DEFAULT_PRESSURE;
+        coords.size = DEFAULT_SIZE;
+        
+        return coords;
+    }
+
+    private static PointerProperties generatePointerProperties(int id) {
+        PointerProperties properties = new PointerProperties();
+
+        properties.id = id;
+        properties.toolType = TOOL_TYPE_FINGER;
+
+        return properties;
+    }
+}
--- lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/Zoomable.java
+++ lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/Zoomable.java
@@ -5,11 +5,14 @@ import android.app.Instrumentation;
 import android.graphics.Point;
 import android.os.SystemClock;
 import android.support.test.espresso.Root;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.ViewActions;
+import android.support.v4.util.Pair;
 import android.view.MotionEvent.PointerCoords;
 import android.view.MotionEvent.PointerProperties;
 import android.view.View;
 
+import com.mindbodyonline.ironhide.Infrastructure.Extensions.ZoomAction;
 import com.mindbodyonline.ironhide.PageObjects.PageObject;
 
 import org.hamcrest.Matcher;
@@ -33,7 +36,15 @@ import static android.view.MotionEvent.obtain;
 @TargetApi(14)
 public class Zoomable<T extends PageObject> extends BaseView<T> {
 
-    private static final int EVENT_MIN_INTERVAL = 1000;
+    private static final int DEFAULT_ZOOM_TIME = 3000;
+
+    private static final int ZOOMS_IN_PER_ALL_ZOOM = 3;
+
+    // number to scroll up from 6am (2 hour increments: 6am-8am-10am-12am)
+    private static final int ZOOMS_OUT_PER_ALL_ZOOM = 6;
+
+    //scrolls to 12am
+    private static final int SWIPES_PER_ALL_ZOOM_IN = 3;
 
     /** @see com.mindbodyonline.ironhide.Infrastructure.IronhideViews.BaseView#BaseView(Class, org.hamcrest.Matcher) */
     public Zoomable(Class<T> type, Matcher<View> selector) {
@@ -57,123 +68,20 @@ public class Zoomable<T extends PageObject> extends BaseView<T> {
         return (Zoomable<E>) super.goesTo(type);
     }
 
-    Point finger1Start;
-    Point finger1End;
-    Point finger2Start;
-    Point finger2End;
-
-    private PointerProperties getPointerProperties(int id, int toolType) {
-        PointerProperties tmp = new PointerProperties();
-        tmp.id = id;
-        tmp.toolType = toolType;
-
-        return tmp;
-    }
-
-    private PointerCoords getPointerCoords(float x, float y, float pressure, float size) {
-        PointerCoords tmp = new PointerCoords();
-        tmp.x = x;
-        tmp.y = y;
-        tmp.pressure = pressure;
-        tmp.size = size;
-
-        return tmp;
-    }
-
-    private void pointerSync(long downTime, long eventTime, int action, int pointerCount,
-                             PointerProperties[] properties,
-                             PointerCoords[] coords, Instrumentation inst) {
-        inst.sendPointerSync(obtain(downTime, eventTime, action, pointerCount, properties, coords, 0, 0, 1, 1, 0, 0, 0, 0));
-    }
-
-    // TODO 71
-    public void generateZoomGesture(Instrumentation inst,
-                                    long startTime, boolean ifMove, Point startPoint1,
-                                    Point startPoint2, Point endPoint1,
-                                    Point endPoint2, int duration) {
-
-        if (inst == null
-                || startPoint1 == null
-                || (ifMove && endPoint1 == null)) {
-            return;
-        }
-
-        long eventTime = startTime;
-
-        FPoint event1 = new FPoint(startPoint1);
-        FPoint event2 = new FPoint(startPoint2);
-
-        // specify the property for the two touch points
-        PointerProperties[] properties = new PointerProperties[] {
-                getPointerProperties(0, TOOL_TYPE_FINGER),
-                getPointerProperties(1, TOOL_TYPE_FINGER)
-        };
-
-        //specify the coordinations of the two touch points
-        //NOTE: you MUST set the pressure and size value, or it doesn't work
-        PointerCoords[] coords = new PointerCoords[] {
-                getPointerCoords(event1.x, event1.y, 1, 1),
-                getPointerCoords(event2.x, event2.y, 1, 1)
-        };
-        
-        int action2Mod = properties[1].id << ACTION_POINTER_INDEX_SHIFT;
-
-        // step 1 - send ACTION_DOWN event of start points
-        pointerSync(startTime, eventTime, ACTION_DOWN, 1, properties, coords, inst);
-        pointerSync(startTime, eventTime, ACTION_POINTER_DOWN + action2Mod, 2, properties, coords, inst);
-
-        //step 2 - send ACTION_MOVE of two middle points until reach the end points
-        if (ifMove) {
-            int moveEventNumber = duration / EVENT_MIN_INTERVAL;
-
-            FPoint step1 = new FPoint(endPoint1);
-            step1.offset(-startPoint1.x, -startPoint1.y);
-            step1.scale(1 / moveEventNumber);
-
-            FPoint step2 = new FPoint(endPoint2);
-            step2.offset(-startPoint2.x, -startPoint2.y);
-            step2.scale(1 / moveEventNumber);
-
-            while (moveEventNumber-- > 0) {
-                // update the move events
-                eventTime += EVENT_MIN_INTERVAL;
-                event1.offset(step1.x, step1.y);
-                event1.offset(step2.x, step2.y);
-
-                event1.putXY(coords[0]);
-                event2.putXY(coords[1]);
-
-                pointerSync(startTime, eventTime, ACTION_MOVE, 2, properties, coords, inst);
-            }
-        }
-
-        //step 3 - send ACTION_POINTER_2_UP of two end points
-        new FPoint(endPoint1).putXY(coords[0]);
-        new FPoint(endPoint2).putXY(coords[1]);
-
-        eventTime += EVENT_MIN_INTERVAL;
-        pointerSync(startTime, eventTime, ACTION_POINTER_UP + action2Mod, 2, properties, coords, inst);
-
-        // step 4 - send ACTION_UP of one end point
-        eventTime += EVENT_MIN_INTERVAL;
-        pointerSync(startTime, eventTime, ACTION_UP, 1, properties, coords, inst);
-    }
+    private static Point finger1Start;
+    private static Point finger1End;
+    private static Point finger2Start;
+    private static Point finger2End;
 
     public T zoomAllIn(int phoneMaxX, int phoneMaxY){
-
-        int zoomInNum = 6;
-
-        // number to scroll up from 6am (2 hour increments: 6am-8am-10am-12am)
-        int swipeDownNum = 3; //scrolls to 12am
-
         finger1Start = new Point(phoneMaxX/2, phoneMaxY/2);
         finger1End = new Point(phoneMaxX/2, phoneMaxY/3);
-        finger2Start = finger1Start;
+        finger2Start = new Point(finger1Start);
         finger2End = new Point(phoneMaxX/2, phoneMaxY);
 
-        zoom(zoomInNum);
+        zoom(ZOOMS_IN_PER_ALL_ZOOM);
 
-        while (swipeDownNum-- > 0)
+        for (int i = 0; i < SWIPES_PER_ALL_ZOOM_IN; i++)
             performAction(ViewActions.swipeDown());
 
         return returnGeneric();
@@ -181,53 +89,22 @@ public class Zoomable<T extends PageObject> extends BaseView<T> {
     }
 
     public T zoomAllOut(int phoneMaxX, int phoneMaxY){
-
-        int zoomOutNum = 3;
-
         finger1Start = new Point(phoneMaxX/2, phoneMaxY/3);
         finger1End = new Point(phoneMaxX/2, phoneMaxY/2);
         finger2Start = new Point(phoneMaxX/2, phoneMaxY);
-        finger2End = finger1End;
+        finger2End = new Point(finger1End);
 
-        return this.zoom(zoomOutNum);
+        return zoom(ZOOMS_OUT_PER_ALL_ZOOM);
     }
 
-    public T zoom(int numTimes) {
+    // changed this one to private since we will have uninitialized variables if people black box this function
+    private T zoom(int numTimes) {
+        ViewAction zoom = new ZoomAction(Pair.create(finger1Start, finger2Start), Pair.create(finger1End, finger2End), DEFAULT_ZOOM_TIME);
 
-        Instrumentation instr = new Instrumentation();
-
-        for (int i = 0; i < numTimes; i++)
-            generateZoomGesture(instr, SystemClock.uptimeMillis(), true, finger1Start, finger2Start, finger1End, finger2End, 3000);
+        while (numTimes-- > 0)
+            performAction(zoom);
 
         return returnGeneric();
-    }
-
-    private class FPoint {
-        float x, y;
-
-        public FPoint(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public FPoint(Point integerPoint) {
-            this(integerPoint.x, integerPoint.y);
-        }
-
-        public void offset(float x, float y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public void scale(float scale) {
-            this.x *= scale;
-            this.y *= scale;
-        }
-
-        public void putXY(PointerCoords coords) {
-            coords.x = this.x;
-            coords.y = this.y;
-        }
     }
 
     /**
