bd6462b300736edd1ce80438c7ebf6472ede2665
Greg Sawers <gregory.sawers@mindbodyonline.com>
2015-02-13 13:55:41 -0800

    Update Recycler.java

--- lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/Recycler.java
+++ lib/src/main/java/com/mindbodyonline/ironhide/Infrastructure/IronhideViews/Recycler.java
@@ -25,7 +25,7 @@ public class Recycler<T extends PageObject> extends BaseView<T> {
     }
 
     /** @see BaseView#BaseView(Class, int) */
-    public Recycler(Class<T> type,int resourceId) {
+    public Recycler(Class<T> type, int resourceId) {
         super(type, resourceId);
     }
 
